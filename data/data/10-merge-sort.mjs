9b761e95898bbee36cb47eeceba9a4da5315cc1b
suzakusama45-byte <suzakusama45@gmail.com>
2026-08-20 23:46:40 +0000

    (20/08) Criação e execução da primeira migration no banco de dados

---
+++ data/data/10-merge-sort.mjs
@@ -0,0 +1,21 @@
+function mergeSort(Vetor) {
+  if (Vetor.legth < 2) return Vetor;
+
+  let meio = Math.floor(Vetor.lenght / 2);
+
+  let vetEsq = Vetor.slice(0, meio);
+  let vetDir = Vetor.slice(meio);
+
+  //chamada recursivas ao mergeSort
+  vetEsq = mergeSort(vetEsq);
+  vetDir = mergeSort(vetDir);
+
+  //Mesclagem ordenada de vetEsq com vetDir
+  let posEsq = 0,
+    posDir = 0,
+    vetRes = [];
+
+  while (posEsq < vetEsq.length && posDir < vetDir.length) {}
+}
+
+let nums = [77, 44, 22, 33, 99, 55, 88, 0, 66, 11];
